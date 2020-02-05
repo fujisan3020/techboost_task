8e9492781c7cb9d4c18d621d93edb40e04af803e
fujisan <gashigashi3040@gmail.com>
2020-02-05 14:56:23 +0900

    課題の2と4を訂正

--- No21.php
+++ No21.php
@@ -44,6 +44,7 @@ if ($name == "Yusuke") {
 }
 
 //課題2
+$total = 0;
 for ($i=0; $i <= 10000 ; $i++) {
   $total += $i;
 }
@@ -62,7 +63,7 @@ $start = 1;
 /* for文の終わりの値を定義する */
 $end = 100;
 
-for($i = $start; $i < $end; $i++){
+for($i = $start; $i <= $end; $i++){
 
   // 5で割り切れたら{}内を実行する
   if($i % 5 == 0){
